4698a695a06cad39320b2f8e4ecf76f32867ddc6
kameme511 <jagaimo0511@gmail.com>
2021-03-08 00:13:24 +0900

    Create pass_failure.php

---
+++ pass_failure.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<?php require_once 'private/database.php'; ?>
+<html lang="ja">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>onegai</title>
+    <nav role="navigation">
+    	<ul>
+  　　  <li><a href="register.php">会員登録</a></li>
+  　　  <li><a href="login.php">ログイン</a></li>
+    	</ul>
+		</nav>
+</head>
+  <body>
+    <header>
+      <h1>ユーザー名、またはパスワードに誤りがあります。<br>再度お願いします。</h1>
+    </header>
+
+    <div class="center-block">
+      <a href="#" class="btn btn-success">ボタン</a>
+      <a href="#" class="btn btn-warning">ボタン</a>
+      </div>
+  </body>
+</html>
