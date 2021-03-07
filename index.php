0eec765290f60c52d210efa87234251b39329277
kameme511 <jagaimo0511@gmail.com>
2021-03-08 00:22:00 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<?php require_once 'private/database.php'; ?>
+<html lang="ja">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>onegai</title>
+    <nav role="navigation">
+    	<ul>
+    		<li id="menu-item-81" class=" "><a href="">About</a></li>
+    	</ul>
+		</nav>
+</head>
+  <body>
+    <header>
+      <h1>SNS</h1>
+    </header>
+
+    <div class="center-block">
+      <a href="register.php" class="btn btn-success">会員登録</a>
+      <a href="login.php" class="btn btn-warning">ログイン</a>
+      </div>
+  </body>
+</html>
