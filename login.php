b0d77edfe5699180026ef283ef94e86f8e9c93be
kameme511 <jagaimo0511@gmail.com>
2021-03-29 19:02:00 +0900

    go

--- login.php
+++ login.php
@@ -1,10 +1,10 @@
 <?php
+require_once 'private/bootstrap.php';
 session_start();
 $token = uniqid(dechex(random_int(0, 255)));
 $_SESSION['token'] = $token;
 ?>
 <html lang="ja">
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
